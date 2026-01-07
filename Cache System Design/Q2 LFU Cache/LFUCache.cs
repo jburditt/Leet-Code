d4dc9a6cda0bf029203dd7bb82eee21624f4b01c
Jebb Burditt <jebb.burditt@gmail.com>
2026-01-07 13:46:43 -0800

    work in progress of system design question 2 lfu cache

---
+++ Cache System Design/Q2 LFU Cache/LFUCache.cs
@@ -0,0 +1,112 @@
+﻿namespace Tests.Cache_System_Design.LFU_Cache;
+
+public class LFUCache
+{
+    private HashedLinkedList _cache;
+    private int _capacity;
+
+    public LFUCache(int capacity)
+    {
+        _capacity = capacity;
+        _cache = new(capacity);
+    }
+
+    public int Get(int key)
+    {
+        return _cache.Get(key);
+    }
+
+    public void Put(int key, int value)
+    {
+        _cache.Put(key, value);
+    }
+}
+
+public class HashedLinkedList
+{
+    private Dictionary<int, LinkedListNode<LFUNode>> _cache = new();
+    private LinkedList<LFUNode> _list = new();
+    private int _capacity;
+
+    public HashedLinkedList(int capacity)
+    {
+        _capacity = capacity;
+    }
+
+    public int Get(int key)
+    {
+        if (_cache.ContainsKey(key))
+        {
+            Update(key);
+            return _cache[key].Value.Value;
+        }
+        else
+            return -1;
+    }
+
+    public void Put(int key, int value)
+    {
+        if (_cache.ContainsKey(key))
+        {
+            Update(key);
+        }
+        else
+        {
+            Insert(key, value);
+            // check capacity
+            if (_list.Count > _capacity)
+            {
+                _cache.Remove(_list.Last.Value.Key);
+                _list.RemoveLast();
+            }
+        }
+    }
+
+    private void Update(int key)
+    {
+        var node = _cache[key];
+        var value = node.Value;
+        value.Count++;
+        value.Timestamp = DateTime.Now.Ticks;
+
+        // check if this node should be promoted
+        if (node.Next != null 
+            && (node.Next.Value.Count > node.Value.Count 
+                || (node.Next.Value.Count == node.Value.Count && node.Next.Value.Timestamp > node.Value.Timestamp)))
+        {
+            // swap values
+            _cache[node.Value.Key] = node.Next;
+            _cache[node.Next.Value.Key] = node;
+            Swap(node, node.Next);
+        } else
+            _cache[key] = node;
+    }
+
+    private void Insert(int key, int value)
+    {
+        var node = new LFUNode(key, value, 1, DateTime.Now.Ticks);
+        _list.AddFirst(node);
+        _cache[key] = _list.First;
+    }
+
+    public static void Swap(LinkedListNode<LFUNode> first, LinkedListNode<LFUNode> second)
+    {
+        var tmp = first.Value;
+        first.Value = second.Value;
+        second.Value = tmp;
+    }
+}
+
+public struct LFUNode
+{
+    public int Key, Value, Count;
+    public long Timestamp;
+
+    public LFUNode(int key, int value, int count, long timestamp)
+    {
+        Key = key;
+        Value = value;
+        Count = count;
+        Timestamp = timestamp;
+    }
+}
---
+++ Cache System Design/Q2 LFU Cache/Test.cs
@@ -0,0 +1,61 @@
+﻿namespace Tests.Cache_System_Design.LFU_Cache;
+
+[TestClass]
+public class Test
+{
+
+    // Q1. LRU Cache
+    [TestMethod]
+    public void Test_Case_1()
+    {
+        var lFUCache = new LFUCache(2);
+        lFUCache.Put(1, 1); // cache is {1=1}
+        lFUCache.Put(2, 2); // cache is {1=1, 2=2}
+        var result = lFUCache.Get(1);    
+        Assert.AreEqual(1, result);
+
+        lFUCache.Put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+        result = lFUCache.Get(2);    // returns -1 (not found)
+        Assert.AreEqual(-1, result);
+
+        result = lFUCache.Get(3);    // cache is {1=1, 3=3}
+        Assert.AreEqual(3, result);
+
+        lFUCache.Put(4, 4); // LFU key was 1, evicts key 1, cache is {3=3, 4=4}
+        result = lFUCache.Get(1);    // return -1 (not found)
+        Assert.AreEqual(-1, result);
+
+        result = lFUCache.Get(3);    // cache is {4=4, 3=3}
+        Assert.AreEqual(3, result);
+
+        result = lFUCache.Get(4);    // return 4
+        Assert.AreEqual(4, result);
+    }
+
+    [TestMethod]
+    public void Test_Submission_Case_11()
+    {
+        var lFUCache = new LFUCache(3);
+        lFUCache.Put(2, 2);             // cache is {2=2,1}
+        lFUCache.Put(1, 1);             // cache is {1=1,1; 2=2,1}
+        var result = lFUCache.Get(2);   // cache is {1=1,1; 2=2,2}
+        Assert.AreEqual(2, result);
+
+        result = lFUCache.Get(1);
+        Assert.AreEqual(1, result);     // cache is {2=2,2; 1=1,2}
+
+        lFUCache.Put(3, 3);             // cache is {3=3,1; 2=2,2; 1=1,2}
+        lFUCache.Put(4, 4);             // evicts key 3, cache is {4=4,1; 2=2,2; 1=1,2}
+        result = lFUCache.Get(3);
+        Assert.AreEqual(-1, result);
+
+        result = lFUCache.Get(2);
+        Assert.AreEqual(2, result);
+
+        result = lFUCache.Get(1);
+        Assert.AreEqual(1, result);
+
+        result = lFUCache.Get(4);
+        Assert.AreEqual(4, result);
+    }
+}
