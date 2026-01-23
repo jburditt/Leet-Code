282c57f481549b84410eae822aa896e799b550de
Jebb Burditt <jebb.burditt@gmail.com>
2026-01-23 14:16:16 -0800

    accepted submission but slower

---
+++ Cache System Design/Data Stream Processing/Q1 Kth Largest Element in a Stream/KthLargest_ArrayCopy.cs
@@ -0,0 +1,97 @@
+﻿namespace DataStreamProcessing.Q1;
+
+// 33 ms beats 18.11%
+// 70.67 MB beats 7.61%
+public class KthLargest_ArrayCopy
+{
+    private SortedList<int> _scores;
+
+    public KthLargest_ArrayCopy(int k, int[] nums)
+    {
+        _scores = new SortedList<int>(k, nums);
+    }
+
+    public int Add(int val)
+    {
+        if (!_scores.Full || val > _scores.Lowest)
+            _scores.Add(val);
+        return _scores.Lowest;
+    }
+
+    public class SortedList<T>
+    {
+        private int _capacity;
+        private int[] _list;
+
+        public SortedList(int capacity, int[] nums)
+        {
+            _capacity = capacity;
+            var temp = new List<int>(nums);
+            var length = Math.Min(temp.Count, capacity);
+            _list = new int[length];
+            temp.Sort();
+            Array.Copy(temp.TakeLast(capacity).ToArray(), _list, length);
+        }
+
+        public int Lowest => _list.First();
+        public bool Full => _list?.Count() >= _capacity;
+
+        public int Add(int val)
+        {
+            // find the pivot at index p
+            int pivot = BinarySearchHigherValue(val);
+            // remove the first element if over capacity
+            if (_list.Count() == _capacity && pivot > 0)
+            {
+                Array.Copy(_list, 1, _list, 0, pivot);
+                _list[pivot] = val;
+            }
+            else if (_list.Length < _capacity)
+            {
+                var temp = _list;
+                _list = new int[_list.Length + 1];
+                if (_list.Length == 1)
+                {
+                    _list[0] = val;
+                }
+                else
+                {
+                    Array.Copy(temp, _list, _list.Length - 1);
+                    // shift pivot item over
+                    _list[pivot + 2] = _list[pivot + 1];
+                    // add new item
+                    _list[pivot + 1] = val;
+                }
+                return Lowest;
+            }
+            else
+            {
+                if (pivot < 0)
+                    pivot = 0;
+                _list[pivot] = val;
+            }
+            return Lowest;
+        }
+
+        private int BinarySearchHigherValue(int val)
+        {
+            int low = 0;
+            int high = _list.Length - 1;
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if (_list[mid] == val)
+                    return mid;
+
+                if (_list[mid] > val)
+                    high = mid - 1;
+
+                if (_list[mid] < val)
+                    low = mid + 1;
+
+            }
+            return high;
+        }
+    }
+}
--- Cache System Design/Data Stream Processing/Q1 Kth Largest Element in a Stream/Test.cs
+++ Cache System Design/Data Stream Processing/Q1 Kth Largest Element in a Stream/Test.cs
@@ -76,70 +76,30 @@ public class Test
     [TestMethod]
     public void Test_Case_5()
     {
-        var kthLargest = new KthLargest(9, new int[] { 2, 4, 5, 8, 10, 12, 15, 18, 20, 25 });
-        var result = kthLargest.Add(3);  // return 4
+        var kthLargest = new KthLargest(7, new int[] { -10, 1, 3, 1, 4, 10, 3, 9, 4, 5, 1});
+        var result = kthLargest.Add(3);  // return 3
+        Assert.AreEqual(3, result);
+        result = kthLargest.Add(2);      // return 3
+        Assert.AreEqual(3, result);
+        result = kthLargest.Add(3);     // return 3
+        Assert.AreEqual(3, result);
+        result = kthLargest.Add(1);
+        Assert.AreEqual(3, result);
+        result = kthLargest.Add(2);
+        Assert.AreEqual(3, result);
+        result = kthLargest.Add(4);
+        Assert.AreEqual(3, result);
+        result = kthLargest.Add(5);
         Assert.AreEqual(4, result);
-        result = kthLargest.Add(7);      // return 5
+        result = kthLargest.Add(5);
+        Assert.AreEqual(4, result);
+        result = kthLargest.Add(6);
+        Assert.AreEqual(4, result);
+        result = kthLargest.Add(7);
         Assert.AreEqual(5, result);
-        result = kthLargest.Add(9);     // return 7
-        Assert.AreEqual(7, result);
-        result = kthLargest.Add(11);      // return 8
-        Assert.AreEqual(8, result);
-        result = kthLargest.Add(6);      // return 8
-        Assert.AreEqual(8, result);
-    }
-
-    [TestMethod]
-    public void Test_Case_8()
-    {
-        var kthLargest = new KthLargest(3, new int[] { 5, 6, 2, 4, 1, 3 });
-        var result = kthLargest.Add(7);  // return 5
+        result = kthLargest.Add(7);
         Assert.AreEqual(5, result);
-        result = kthLargest.Add(0);      // return 5
+        result = kthLargest.Add(8);
         Assert.AreEqual(5, result);
-        result = kthLargest.Add(8);     // return 6
-        Assert.AreEqual(6, result);
-    }
-
-    [TestMethod]
-    public void Test_Case_9()
-    {
-        var kthLargest = new KthLargest(4, new int[] { 1, 2, 3 });
-        var result = kthLargest.Add(4);  // return 1
-        Assert.AreEqual(1, result);
-        result = kthLargest.Add(5);      // return 2
-        Assert.AreEqual(2, result);
-        result = kthLargest.Add(6);     // return 3
-        Assert.AreEqual(3, result);
-        result = kthLargest.Add(0);      // return 3
-        Assert.AreEqual(3, result);
-    }
-
-    [TestMethod]
-    public void Test_Case_10()
-    {
-        var kthLargest = new KthLargest(1, new int[] { -1 });
-        var result = kthLargest.Add(1);  // return 1
-        Assert.AreEqual(1, result);
-        result = kthLargest.Add(-2);      // return 1
-        Assert.AreEqual(1, result);
-        result = kthLargest.Add(-3);     // return 1
-        Assert.AreEqual(1, result);
-        result = kthLargest.Add(-4);      // return 1
-        Assert.AreEqual(1, result);
-        result = kthLargest.Add(0);      // return 1
-        Assert.AreEqual(1, result);
-    }
-
-    [TestMethod]
-    public void Test_Case_11()
-    {
-        var kthLargest = new KthLargest(2, new int[] { -1 });
-        var result = kthLargest.Add(-2);  // return -2
-        Assert.AreEqual(-2, result);
-        result = kthLargest.Add(0);      // return -1
-        Assert.AreEqual(-1, result);
-        result = kthLargest.Add(1);     // return 0
-        Assert.AreEqual(0, result);
     }
 }
