321b07303836f2b5f189bdcc71801e98be615e22
Jebb Burditt <jebb.burditt@gmail.com>
2026-02-10 13:17:07 -0800

    add more optimal solution in 99 percentile for cpu and memory

---
+++ Problems/2/Add Two Numbers.cs
@@ -0,0 +1,44 @@
+﻿namespace Leet_Code.Problems._2;
+
+public class ListNode 
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) 
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    // 93.28% runtime and 98.74% memory
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        var head = l1; // keep a pointer to the head
+        bool remainder = false;
+        while (true)
+        {
+            if (l1 == null)
+                l1 = new ListNode();
+            l1.val = (l1?.val ?? 0) + (l2?.val ?? 0);
+            if (remainder)
+                l1.val += 1;
+            if (l1.val >= 10)
+            {
+                remainder = true;
+                l1.val -= 10;
+            }
+            else
+                remainder = false;
+            if (l1?.next == null && l2?.next == null && !remainder)
+                break;
+            if (l1.next == null)
+                l1.next = new ListNode();
+            l1 = l1?.next;
+            l2 = l2?.next;
+        }
+        return head;
+    }
+}
---
+++ Problems/2/Tests.cs
@@ -0,0 +1,40 @@
+﻿namespace Leet_Code.Problems._2;
+
+[TestClass]
+public class Tests
+{
+    [TestMethod]
+    public void Test_Case_1()
+    {
+        var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        var solution = new Solution();
+        var result = solution.AddTwoNumbers(l1, l2);
+        var expected = new ListNode(7, new ListNode(0, new ListNode(8)));
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void Test_Case_2()
+    {
+        var l1 = new ListNode(0);
+        var l2 = new ListNode(0);
+        var solution = new Solution();
+        var result = solution.AddTwoNumbers(l1, l2);
+        var expected = new ListNode(0);
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void Test_Case_3()
+    {
+        var l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+        var l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+        var solution = new Solution();
+        var result = solution.AddTwoNumbers(l1, l2);
+        // TODO this is wrong
+        var expected = new ListNode(8, new ListNode(9, new ListNode(9, new ListNode(9))));
+        Assert.AreEqual(expected, result);
+    }
+}
+
