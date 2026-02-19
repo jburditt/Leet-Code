ff797b0cf0a553c6ea151b44e51e5821c53e3b7c
Jebb Burditt <jebb.burditt@gmail.com>
2026-02-19 11:09:09 -0800

    code cleanup problem 3

--- Problems/3/Longest Substring Without Repeating Characters.cs
+++ Problems/3/Longest Substring Without Repeating Characters.cs
@@ -4,31 +4,32 @@ public class Solution
 {
     // NOTE I'll be honest here, this one required looking up the "Sliding Window" solution as a concept and then that was enough to develop the solution on my own
     // in other words, I cheated, but only a little bit
-    //  minutes, beats .0%
+    // 5 ms beats 77.63%, 43.71 MB beats 77.89%
     public int LengthOfLongestSubstring(string s)
     {
-        // TODO change to HashSet
-        var dict = new Dictionary<char, int>();
+        var charset = new HashSet<char>();
         int currentSubstringLength = 0;
         var maxSubstringLength = 0;
         var left = 0;
-        if (s.Length == 1)
-            return 1;
 
+        // move the right side of the window
         for (var right = 0; right < s.Length; right++)
         {
-            while (dict.ContainsKey(s[right]))
+            // move the left side of the window until we reach the duplicate character
+            while (charset.Contains(s[right]))
             {
-                dict.Remove(s[left]);
+                charset.Remove(s[left]);
                 left++;
             }
 
+            // save largest window length
             currentSubstringLength = right - left + 1;
             if (currentSubstringLength > maxSubstringLength)
                 maxSubstringLength = currentSubstringLength;
 
-            dict.Add(s[right], right);
+            charset.Add(s[right]);
         }
-        return Math.Max(maxSubstringLength, s.Length - left);
+
+        return maxSubstringLength;
     }
 }
